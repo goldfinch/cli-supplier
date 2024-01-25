6fc9ee88c26a7203b48d871eb72e1952b99204bf
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-01-25 14:58:29 +1300

    WIP

--- src/Controllers/CLISupplierController.php
+++ src/Controllers/CLISupplierController.php
@@ -2,14 +2,22 @@
 
 namespace Goldfinch\CLISupplier\Controllers;
 
+use Exception;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Config;
 
 class CLISupplierController extends Controller
 {
-    private static $url_handlers = [];
+    private static $url_handlers = [
+        '$Action' => 'runRegisteredSupplies',
+    ];
 
-    private static $allowed_actions = [];
+    private static $allowed_actions = [
+        'runRegisteredSupplies',
+    ];
+
+    private static $registered_supplies = [];
 
     // private static $deny_non_cli = true;
 
@@ -26,5 +34,24 @@ class CLISupplierController extends Controller
         print_r(json_encode($routes));
         exit;
         // return 11;
+    }
+
+    public function runRegisteredSupplies(HTTPRequest $request)
+    {
+        $controllerClass = null;
+
+        $baseUrlPart = $request->param('Action');
+        $reg = Config::inst()->get(static::class, 'registered_supplies');
+        if (isset($reg[$baseUrlPart])) {
+            $controllerClass = $reg[$baseUrlPart]['controller'];
+        }
+
+        if ($controllerClass && class_exists($controllerClass ?? '')) {
+            return $controllerClass::create();
+        }
+
+        $msg = 'Error: no controller registered in ' . static::class . ' for: ' . $request->param('Action');
+
+        throw new Exception($msg);
     }
 }
